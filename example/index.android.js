3b0279a893765d3a6a0145c1c8f306b485586221
Wei Jian <wei.jian.coder+github@gmail.com>
2017-05-08 00:02:30 +0800

    test: add test case & example

---
+++ example/index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import Root from './root';
+
+AppRegistry.registerComponent('example', () => Root);
